c09ea10101feaa9e5ab05241370090263bf9554d
Karim Alomari <karim.alomari@gmail.com>
2020-09-17 14:48:31 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,4 +12,16 @@ class Artist
     @@all
   end
   
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+  end
+
+  def genres
+    self.songs.map {|song| song.genre }
+  end
+  
 end
